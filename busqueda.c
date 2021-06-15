fbf0adaafea0139cf820fcf455367545a538397c
delPuer <61361639+delPuer@users.noreply.github.com>
2021-06-15 00:19:12 -0400

    Update busqueda.c

--- busqueda.c
+++ busqueda.c
@@ -4,16 +4,16 @@
 #define MAX 20
 int array[MAX];
 int key,ubi,idHilo;
-int bandera = 0;                                //bandera to indicate that item is found in the array or not
+int bandera = 0;                                
 int current_thread = 0;
-void* ThreadSearch(void* args) {                //This is linear search function. It will be running using all threads
+void* ThreadSearch(void* args) {               
     
     if(bandera == 1)
         pthread_exit(0);
     int num = current_thread++;
     for (int i = num * (MAX / 4); i < ((num + 1) * (MAX / 4)); i++){
         if (array[i] == key){
-            bandera = 1;                        //set bandera if key is found
+            bandera = 1;                        
             ubi = i;
         }
     }
@@ -33,11 +33,11 @@ int main(int argc, char **argv) {
         printf("%d\n",array[i]);
     }
     
-    for (int i = 0; i < tamanho; i++) {         //create multiple threads
+    for (int i = 0; i < tamanho; i++) {        
         pthread_create(&thread[i], NULL, ThreadSearch, (void*)NULL);
     }
     for (int i = 0; i < tamanho; i++) {
-        pthread_join(thread[i], &rest);          //wait untill all of the threads are completed
+        pthread_join(thread[i], &rest);          
         idHilo = thread[i];
     }
     
